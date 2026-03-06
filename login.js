94884d696ad63681108d99a12443e7326a78971d
Hydr2005 <hydrelpro1@gmail.com>
2026-03-06 16:33:27 +0100

    Update redirect URL for Discord OAuth login

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "1479450889654763520";
 
-const REDIRECT = window.location.origin + "/callback.html";
+const REDIRECT = "https://hydr2005.github.io/formulario-abyssal/callback.html";
 
 const url =
 `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT)}&scope=identify`;
@@ -10,3 +10,4 @@ document.getElementById("login").onclick = () => {
 window.location.href = url;
 
 };
+
