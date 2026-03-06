03c0e65ca047bed8573b1af0a159f44cf362d615
Hydr2005 <hydrelpro1@gmail.com>
2026-03-06 16:26:23 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
-const CLIENT_ID = "1479450889654763520"
-const REDIRECT_URI = "https://hydr2005.github.io/formulario-abyssal/callback.html"
+const CLIENT_ID = "1479450889654763520";
+
+const REDIRECT = window.location.origin + "/callback.html";
+
+const url =
+`https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT)}&scope=identify`;
 
 document.getElementById("login").onclick = () => {
 
-const url =
+window.location.href = url;
 
-`https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=identify`
-
-window.location.href = url
-
-}
+};
